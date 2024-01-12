7cbf24589a9cbf13bef98302160d85398092414e
kadir7563483 <148765216+TheTrackOne@users.noreply.github.com>
2024-01-12 09:06:11 +0200

    new

--- Script/Navbar.js
+++ Script/Navbar.js
@@ -2,3 +2,7 @@ document.getElementByClassName("menu-toggle-btn").click(function(){
     this.classList.toggle("fa-times");
     document.getElementByClassName("navigation-menu").classList.toggle("active");
 });
+function ModalOpen (){
+    document.getElementById('phone-1').style.display = 'block';
+    document.getElementById('phone-2').style.display = 'block';
+}
